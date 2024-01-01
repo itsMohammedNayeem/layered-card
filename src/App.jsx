c19bd6df608e7dcc63b9668e738f57d971f1c3b6
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2024-01-01 18:51:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <main>
+        <div className="card">
+          <div className="welcome">Welcome</div>
+          <div className="start-chatting">Start chatting</div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
