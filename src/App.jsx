c5b7dfcd24f6dcf745f001d474dd6327aafcc2ce
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2024-01-02 00:30:15 +0530

    Update .gitignore and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ function App() {
         <div className="welcome bg-gradient-to-r from-cyan-500 to-blue-500 col-span-full row-[1/3] grid grid-rows-subgrid grid-cols-subgrid *:items-center *:justify-center">
           <div className="col-[2/3] py-[1rem]">
             <h1 className="hello text-[1.25rem] font-normal">Heyy!</h1>
-            <p className="text-balance">
+            <p className="text-pretty">
               You can contact me whenever you need help or just curious about
               something.
             </p>
           </div>
         </div>
 
-        <div className="start-chatting bg-white col-[2/3] row-[2/4] *:items-center *:justify-center shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] grid gap-[2rem] pt-[2rem] pb-[4rem] px-[4rem] *:m-0">
-          <h2 className="font-semibold ">Start chatting</h2>
-          <div className="rep-info *:m-0 flex flex-col items-center justify-center">
+        <div className="start-chatting bg-white col-[2/3] row-[2/4] *:items-center *:justify-center shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] grid gap-[2rem] pt-[2rem] pb-[4rem] px-[4rem] ">
+          <h2 className="font-[1rem]">Start chatting</h2>
+          <div className="rep-info flex flex-col space-y-2 items-center justify-center">
             <img
               src="https://avatars.githubusercontent.com/u/127741549?v=4"
               className="size-[6rem] rounded-full max-w-[6rem]"
@@ -27,7 +27,7 @@ function App() {
             >
               Active
             </div>
-            <button className="button rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 h-10 w-40">
+            <button className="button rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 p-[0.75em] cursor-pointer text-[1.05rem] hover:bg-[size:200%] focus:bg-[size:200%] bg-[size:100%] duration-[250ms] hover:scale-[1.05] focus:scale-[1.05] transition-all ease-in-out">
               Send a message
             </button>
           </div>
